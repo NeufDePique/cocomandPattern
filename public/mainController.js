3c44fdf55a5d5af789a544202e3034b2ca7b0dd3
Claire Marche <claire.marche@orange.fr>
2019-12-05 20:34:03 +0100

    Added a menu component

--- public/mainController.js
+++ public/mainController.js
@@ -3,3 +3,8 @@ var main = angular.module('main', []);
 main.controller("mainController", function($scope) {
     $scope.value = Math.random();
 });
+
+main.component("menu", {
+    template: "this is the menu",
+    controller: function MenuController() {}
+});
