ff5fb58c3973ded69bdd46195e85afa673117f9b
Claire Marche <claire.marche@orange.fr>
2019-12-06 00:59:58 +0100

    Fixed?

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var express = require("express");
 var app = express();
-var http = require('http');
-
-var server = http.createServer(app);
-
-// Ecoute sur les websockets
-var io = require('socket.io').listen(http);
-io.set('origins', '*:*');
 
 
-app.use(express.static('public'));
+app.use(express.static('public_html'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public_html/index.html');
@@ -19,125 +12,9 @@ app.get('/secret', function(req, res) {
     res.sendFile(__dirname + '/public_html/secret.html');
 })
 
-<<<<<<< HEAD
-/*app.get('/*', function(req, res) {
-=======
-app.get('/options', function(req, res) {
-    res.sendFile(__dirname + '/public_html/options.html');
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/public_html/error.html');
 })
 
-app.get('/*', function(req, res) {
->>>>>>> c60e11462e0c55873210f3dd2568dc365ddc6472
-    res.sendFile(__dirname + '/public_html/error.html');
-})*/
-
-var server = app.listen(8080);
+app.listen(8080);
 console.log("now listening on port 8080")
-
-
-<<<<<<< HEAD
-=======
-
-// Ecoute sur les websockets
-var io = require('socket.io').listen(server);
-io.set('origins', '*:*');
-
-// Configuration d'express pour utiliser le répertoire "public"
-app.use(express.static('public'));
-// set up to
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public_html/chat.html');
-});
-
-
->>>>>>> c60e11462e0c55873210f3dd2568dc365ddc6472
-/*** Gestion des clients et des connexions ***/
-var clients = {};       // id -> socket
-
-// Quand un client se connecte, on le note dans la console
-io.on('connection', function (socket) {
-
-    // message de debug
-    console.log("Un client s'est connecté");
-    var currentID = null;
-
-    /**
-     *  Doit être la première action après la connexion.
-     *  @param  id  string  l'identifiant saisi par le client
-     */
-    socket.on("login", function(id) {
-        while (clients[id]) {
-            id = id + "(1)";
-        }
-        currentID = id;
-        clients[currentID] = socket;
-
-        console.log("Nouvel utilisateur : " + currentID);
-        // envoi d'un message de bienvenue à ce client
-        socket.emit("bienvenue", id);
-        // envoi aux autres clients
-        socket.broadcast.emit("message", { from: null, to: null, text: currentID + " a rejoint la discussion", date: Date.now() } );
-        // envoi de la nouvelle liste à tous les clients connectés
-        io.sockets.emit("liste", Object.keys(clients));
-    });
-
-
-    /**
-     *  Réception d'un message et transmission à tous.
-     *  @param  msg     Object  le message à transférer à tous
-     */
-    socket.on("message", function(msg) {
-        console.log("Reçu message");
-        // si jamais la date n'existe pas, on la rajoute
-        msg.date = Date.now();
-        // si message privé, envoi seulement au destinataire
-        if (msg.to != null && clients[msg.to] !== undefined) {
-            console.log(" --> message privé");
-            clients[msg.to].emit("message", msg);
-            if (msg.from != msg.to) {
-                socket.emit("message", msg);
-            }
-        }
-        else {
-            console.log(" --> broadcast");
-            io.sockets.emit("message", msg);
-        }
-    });
-
-
-    /**
-     *  Gestion des déconnexions
-     */
-
-    // fermeture
-    socket.on("logout", function() {
-        // si client était identifié (devrait toujours être le cas)
-        if (currentID) {
-            console.log("Sortie de l'utilisateur " + currentID);
-            // envoi de l'information de déconnexion
-            socket.broadcast.emit("message",
-                { from: null, to: null, text: currentID + " a quitté la discussion", date: Date.now() } );
-                // suppression de l'entrée
-            delete clients[currentID];
-            // envoi de la nouvelle liste pour mise à jour
-            socket.broadcast.emit("liste", Object.keys(clients));
-        }
-    });
-
-    // déconnexion de la socket
-    socket.on("disconnect", function(reason) {
-        // si client était identifié
-        if (currentID) {
-            // envoi de l'information de déconnexion
-            socket.broadcast.emit("message",
-                { from: null, to: null, text: currentID + " vient de se déconnecter de l'application", date: Date.now() } );
-                // suppression de l'entrée
-            delete clients[currentID];
-            // envoi de la nouvelle liste pour mise à jour
-            socket.broadcast.emit("liste", Object.keys(clients));
-        }
-        console.log("Client déconnecté");
-    });
-
-
-});
