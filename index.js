9fb22e252fdb964df84bbb028cbd7d641bb4906c
Borde Corentin <cborde414@gmail.com>
2019-12-06 01:48:31 +0100

    add conseils.html

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ app.get('/secret', function(req, res) {
     res.sendFile(__dirname + '/public_html/secret.html');
 })
 
+app.get('/conseils', function(req, res){
+    res.sendFile(__dirname + '/public_html/conseils.html');
+})
+
 app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/public_html/error.html');
 })
 
-app.listen(80);
+app.listen(8080);
 console.log("now listening on port 8080")
